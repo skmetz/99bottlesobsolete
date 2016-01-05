1caab873be591159168a3bcc2f44da1e5463170a
Katrina Owen <katrina.owen@gmail.com>
2016-01-05 06:34:29 -0700

    Hierarchy: delete next_bottle_number temp

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -14,7 +14,6 @@ class Bottles
 
   def verse(number)
     bottle_number = BottleNumber.for(number)
-    next_bottle_number = BottleNumber.for(bottle_number.successor)
 
     "#{bottle_number} of beer on the wall, ".capitalize +
     "#{bottle_number} of beer.\n" +
